b08fac235ec29ec4ca69697840dbe2d987d8b08b
Fernando <flopezramirez@hotmail.com>
2020-07-29 23:39:14 -0500

    Created the initial files

---
+++ surveyHealth/surveyHealth/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for surveyHealth project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'surveyHealth.settings')
+
+application = get_asgi_application()
